c7bd4840f14b22e18d0593802792c43b51adb18f
MOHAN SUDHANDHIAM M <mohansudhandhiram.m2025ece@sece.ac.in>
2026-02-10 05:18:21 +0000

    refactor: stack as local array

--- clg_class/stack/stack_using_array.c
+++ clg_class/stack/stack_using_array.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 #define MAX 6
 
 typedef enum{
@@ -8,158 +9,137 @@ typedef enum{
      empty
 }status;
 
-// int stack[MAX];
-// int top = -1;
 
-// status push(int data);
-// status pop();
-// status peek();
-// status display();
-
-// int main()
-// {
-    
-//     int choice,add;
-    
-//     while(1) 
-//     {
-//         printf("0.exit\n1.create stack\n2.remove from stack\n3.peek at stack\n4.display stack\n");
-//         scanf("%d",&choice);
-//         switch(choice)
-//     {
-//         case 1:
-            
-//             do
-//             {
-//                 int data;
-//                 printf("\nENTER DATA TO ADD TO STACK: ");
-//                 scanf("%d",&data);
-                
-//                 if(push(data) == full)
-//                 {
-//                     printf("STACK FULL!!\n");
-//                     break;
-//                 }
-//                 printf("\n--DATA ADDED!--\n");
-//                 printf("Do you want to add to the stack? (0/1)\n");
-//                 scanf("%d",&add);
-//             }while(add);
-//             break;
-//         case 2:
-            
-//             do
-//             {
-               
-//                 if(pop() == empty)
-//                 {
-//                     printf("STACK EMPTY!!\n");
-//                     break;
-//                 }
-//                 printf("\n---Data removed---");
-//                 printf("\nDo you want to delete another data? (0/1)\n");
-//                 scanf("%d",&add);
-//             }while(add);
-//             break;
-//         case 3:
-            
-//             if(peek() == empty)
-//             {
-//                 printf("STACK EMPTY!!\n");
-//                 break;
-//             }
-//             break;
-//         case 4:
-//             printf("\n---STACK---\n\n");
-            
-//             if(display() == empty)
-//             {
-//                 printf("\nSTACK EMPTY!!\n");
-//                 break;
-//             }
-//             break;
-//         case 0:
-//             return 0;
-//     }
-//     }
-//     return 0;
-
-// }
-
-// status push(int data)
-// {
-//     if(top == MAX-1)
-//     {
-//         return full;
-//     }
-//     top++;
-//     stack[top] = data;
-//     return 0;
-// }
-
-// status display()
-// {
-
-//     if(top == -1)
-//     {
-//         return empty;
-//     }
-//     for(int i = top;i>=0;i--)
-//     {
-//         printf("%d \n",stack[i]);
-
-//     }
-//     return 0;
-// }
-// status pop()
-// {
-//     if(top == -1)
-//     {
-//         return empty;
-//     }
-//     top--;
-//     return 0;
-// }
-// status peek()
-// {
-//     if(top == -1)
-//     {
-//         return empty;
-//     }
-//     printf("%d",stack[top]);
-//     return 0;
-// }
 
 typedef struct 
 {
     int items[MAX];
-    int top = -1;
+    int top;
 }stack;
+status push(stack *s,int data);
+status display(stack *s);
+status initialize_stack(stack *s);
+bool is_empty(stack *s);
+bool is_full(stack *s);
+status peek(stack *s);
+status pop(stack *s);
 
 int main()
 {
     stack stack1;
-    push(&stack1,11);
+    initialize_stack(&stack1);
+    int choice, data;
+    
+    while(1)
+    {
+        printf("--- Stack Menu ---\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Peek\n");
+        printf("4. Display\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        
+        switch(choice)
+        {
+            case 1:
+                printf("Enter data to push: ");
+                scanf("%d", &data);
+                if(push(&stack1, data) == success)
+                    printf("Pushed successfully\n");
+                else
+                    printf("Stack is full\n");
+                break;
+                
+            case 2:
+                if(pop(&stack1) == success)
+                    printf("Popped successfully\n");
+                else
+                    printf("Stack is empty\n");
+                break;
+                
+            case 3:
+                if(peek(&stack1) == empty)
+                    printf("Stack is empty\n");
+                else
+                    printf("\n");
+                break;
+                
+            case 4:
+                if(display(&stack1) == empty)
+                    printf("Stack is empty\n");
+                break;
+                
+            case 5:
+                printf("Exiting...\n");
+                exit(0);
+                
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+    
+    return 0;
+}
+
+status initialize_stack(stack *s)
+{
+    s->top = -1;
+    return success;
 }
 
 status push(stack *s,int data)
 {
-    if(s->top == MAX-1)
+    if(is_full(s))
     {
         return full;
     }
-    top++;
-    s->items[top] = data;
+    s->top++;
+    s->items[s->top] = data;
     return success;
 }
 status display(stack *s)
 {
-    if(s->top == -1)
+    
+    if(is_empty(s))
     {
         return empty;
     }
-    while(s->top >= 0)
+    for (int i = s->top; i >= 0; i--) 
     {
-        printf("%d",s->item[s->top]);
-        (s->top)--;
+        printf("%d\n", s->items[i]);
     }
+    return success;
+}
+
+status pop(stack *s)
+{
+    if(is_empty(s))
+    {
+        return empty;
+    }
+    (s->top)--;
+    return success;
+
+}
+
+bool is_empty(stack *s) 
+{
+    return s->top == -1;
+}
+
+bool is_full(stack *s) 
+{
+    return s->top == MAX - 1;
+}
+
+status peek(stack *s)
+{
+    if(is_empty(s))
+    {
+        return empty;
+    }
+    printf("%d",s->items[s->top]);
+    return success;
 }
