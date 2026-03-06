868cb3c90d284ff8fb969a2680495f558c588bb8
Mohan Sudhandhiram M <mohansudhandhiram.m2025ece@sece.ac.in>
2026-03-06 06:32:04 +0530

    Learn: lowest common ancestor of tree

---
+++ clg_class/boot_camp/lowest_common_ancestor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) 
+{
+    if (root == NULL || root == p || root == q) {
+        return root;
+    }
+    
+    struct TreeNode* left_signal = lowestCommonAncestor(root->left, p, q);
+    struct TreeNode* right_signal = lowestCommonAncestor(root->right, p, q);
+    
+    
+    if (left_signal != NULL && right_signal != NULL) {
+        return root;
+    }
+    return (left_signal != NULL) ? left_signal : right_signal;
+}
